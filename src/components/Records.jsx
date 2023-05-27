bff9ad0703322261b1105205be2737ce93bbfc6f
adenugbamichael <adenugbamicke@gmail.com>
2023-05-27 13:41:36 +0100

    Add state function

--- src/components/Records.jsx
+++ src/components/Records.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Records = () => {
+  const [records, setRecords] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [description, setDescription] = useState("");
+
   return (
-    <div className='container py-4 mx-auto'>
-      <h1 className='uppercase text-2xl'>Records</h1>
+    <div className='container py-2 mx-auto'>
+      <div className='border-2 border-[#3F3F3F] rounded shadow-md px-5 py-8 mb-5'>
+        <h2 className='uppercase text-xl'>Add a Record</h2>
+        <input
+          type='number'
+          className='p-4 py-2 text-sm border-[#3F3F3F] border-2 w-full mt-3'
+          placeholder='Amount?'
+        />
+        <textarea
+          className='p-4 py-2 text-sm border-[#3F3F3F] border-2 w-full mt-3'
+          placeholder='Description of the expense'
+        ></textarea>
+        <button className='nowrap border-[1px] border-solid border-[#aaa] p-7 mt-3 py-2 text-center text-sm shadow-lg'>
+          Add
+        </button>
+      </div>
+      <h2 className='uppercase text-2xl'>Records</h2>
       <table className='table w-full'>
         <thead className='table-header-group bg-[#3F3F3F]'>
           <tr className='table-row'>
