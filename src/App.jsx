25a5638b399f2cd68cc30652caaa67715e62a938
adenugbamichael <adenugbamicke@gmail.com>
2023-05-27 12:00:55 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Records from "./components/Records";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Records />;
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className=' text-center py-20 bg-slate-200'>
+      <h1 className=' text-4xl  '>Book keeping App</h1>
+      <div className='text-lg mt-4'>
+        Keep your everyday expenses record over here{" "}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
